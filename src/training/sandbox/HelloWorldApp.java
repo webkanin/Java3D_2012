e500e738bc93aa4832874388d129381be7ba3ab7
Marko Sundic <msundic@gmail.com>
2012-01-20 13:42:20 +0100

    First commit

---
+++ src/training/sandbox/HelloWorldApp.java
@@ -0,0 +1,10 @@
+package training.sandbox;
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
